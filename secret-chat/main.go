074a0251448d666f1a89a0d8936c0aa55045d88d
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-14 22:24:18 +0700

    minor changes

--- secret-chat/main.go
+++ secret-chat/main.go
@@ -53,24 +53,16 @@ func main() {
 	fmt.Println("Server is listening on port 8080")
 
 	for {
-		// Accept incoming connections
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error:", err)
 			continue
 		}
 
-		// Handle client connection in a goroutine
 		go handleClient(conn)
 	}
 }
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
-
-	// Read and process data from the client
-	// ...
-
-	// Write data back to the client
-	// ...
 }
